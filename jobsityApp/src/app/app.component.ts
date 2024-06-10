5b056d120338444f7b902186162be955ab977f6c
unknown <fmmolinero@gmail.com>
2024-06-10 14:47:33 -0300

    adding bootstrap to general styles and creating components

--- jobsityApp/src/app/app.component.ts
+++ jobsityApp/src/app/app.component.ts
@@ -1,10 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { ProgramComponent } from './components/program/program.component';
+import { SectionComponent } from './components/section/section.component';
+import { ColumnsComponent } from './components/columns/columns.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+
+    HeaderComponent,
+    BannerComponent,
+    ProgramComponent,
+    SectionComponent,
+    ColumnsComponent
+    
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
