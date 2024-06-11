14bf0d7e5e83ffacfb99677ccf010525ac7f246f
unknown <fmmolinero@gmail.com>
2024-06-11 17:40:24 -0300

    adding programs component

--- jobsityApp/src/app/components/program/program.component.ts
+++ jobsityApp/src/app/components/program/program.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCalendar, faCheck, faClock, faStar } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-program',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './program.component.html',
   styleUrl: './program.component.scss'
 })
 export class ProgramComponent {
+  faStar = faStar;
+  faClock = faClock;
+  faCalendar = faCalendar;
+  faIconCheck = faCheck;
+  
+  activeItem: string = 'basic';
 
+  setActive(item: string) {
+    this.activeItem = item;
+  }
 }
