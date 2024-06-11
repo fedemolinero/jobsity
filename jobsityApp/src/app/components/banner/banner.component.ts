7cfe7ecf6da8e94d801e1e74b6159ee8609a7c63
unknown <fmmolinero@gmail.com>
2024-06-11 11:45:47 -0300

    adding fonts and fontawesome icons

--- jobsityApp/src/app/components/banner/banner.component.ts
+++ jobsityApp/src/app/components/banner/banner.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCoffee, faSortDown } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-banner',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './banner.component.html',
   styleUrl: './banner.component.scss'
 })
 export class BannerComponent {
-
+  faCoffee = faSortDown;
 }
