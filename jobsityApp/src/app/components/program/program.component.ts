c07818a01070de12d53b84755784edc9e2664adb
unknown <fmmolinero@gmail.com>
2024-06-12 18:05:17 -0300

    adding background on program

--- jobsityApp/src/app/components/program/program.component.ts
+++ jobsityApp/src/app/components/program/program.component.ts
@@ -33,6 +33,37 @@ export class ProgramComponent {
 
   selectedTab: number = 1;
 
+  coffeeTopics: string[] = [
+    'Legend of the origin of coffee.',
+    'Chronicle of the spread of the coffee tree',
+    'What is coffee?',
+    'The structure of the coffee berry',
+    'Arabica and Robusta',
+    'Coffee Belt land',
+    'Fruiting coffee tree',
+    'Seven steps of the coffee plantation to the consumer',
+    'Breeding and hybridization of coffee tree'
+  ];
+
+  coffeeProductionTopics: string[] = [
+    'Methods for collecting coffee berries',
+    'Processing collected berries',
+    'Calibration and sorting',
+    'Some methods of classification and labeling of coffee beans.',
+    'Formation of grade names.',
+    'Storage of green (not fried) Coffee'
+  ];
+
+  roastingTopics: string[] = [
+    'The technology of roasting coffee beans.',
+    'Changes in the beans',
+    'Stages and types of roasting',
+    'Roasted coffee. Monosorta and hood.',
+    'Decaffeination of coffee.',
+    'Flavoring',
+    'Storage roasted coffee'
+  ];
+
   selectTab(tab: number) {
     this.selectedTab = tab;
   }
