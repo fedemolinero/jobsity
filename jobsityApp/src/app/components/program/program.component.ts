515223b3adb53f5e2dd3dfa2a4920e081ff4e04f
unknown <fmmolinero@gmail.com>
2024-06-12 19:47:24 -0300

    adding style for divider

--- jobsityApp/src/app/components/program/program.component.ts
+++ jobsityApp/src/app/components/program/program.component.ts
@@ -3,9 +3,9 @@ import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faCalendar, faCheck, faClock, faStar, faTimes } from '@fortawesome/free-solid-svg-icons';
 import { IncludesComponent } from './includes/includes.component';
-import { Tab1Component } from './tab1/tab1.component';
-import { Tab2Component } from './tab2/tab2.component';
-import { Tab3Component } from './tab3/tab3.component';
+import { Tab1Component } from '../../tab1/tab1.component';
+import { Tab2Component } from '../../tab2/tab2.component';
+import { Tab3Component } from '../../tab3/tab3.component';
 
 @Component({
   selector: 'app-program',
