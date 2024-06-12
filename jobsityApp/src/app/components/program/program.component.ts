d46db586d4b7c71f4670e9cbbcf8c855fe6dec55
unknown <fmmolinero@gmail.com>
2024-06-12 16:48:00 -0300

    adding tabs and styles

--- jobsityApp/src/app/components/program/program.component.ts
+++ jobsityApp/src/app/components/program/program.component.ts
@@ -3,11 +3,19 @@ import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faCalendar, faCheck, faClock, faStar, faTimes } from '@fortawesome/free-solid-svg-icons';
 import { IncludesComponent } from './includes/includes.component';
+import { Tab1Component } from './tab1/tab1.component';
+import { Tab2Component } from './tab2/tab2.component';
+import { Tab3Component } from './tab3/tab3.component';
 
 @Component({
   selector: 'app-program',
   standalone: true,
-  imports: [FontAwesomeModule, CommonModule, IncludesComponent],
+  imports: [
+    FontAwesomeModule,
+    CommonModule,
+    IncludesComponent,
+    Tab1Component, Tab2Component, Tab3Component
+  ],
   templateUrl: './program.component.html',
   styleUrl: './program.component.scss'
 })
@@ -21,6 +29,12 @@ export class ProgramComponent {
 
   setActive(item: string) {
     this.activeItem = item;
+  }
+
+  selectedTab: number = 1;
+
+  selectTab(tab: number) {
+    this.selectedTab = tab;
   }
 
 }
