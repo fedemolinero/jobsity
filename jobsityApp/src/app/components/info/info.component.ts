3a06212c9043da9af0fbb0dcfeea71df8545826b
unknown <fmmolinero@gmail.com>
2024-06-11 22:05:14 -0300

    adding info section components

--- jobsityApp/src/app/components/info/info.component.ts
+++ jobsityApp/src/app/components/info/info.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faEnvelope, faLocationDot, faPhone, faStar } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-info',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './info.component.html',
   styleUrl: './info.component.scss'
 })
 export class InfoComponent {
-
+  faPhone = faPhone;
+  faLocationDot = faLocationDot;
+  faEnvelope = faEnvelope;
 }
