eb38084cf25d599a51421e889d8b83311a3c46b5
unknown <fmmolinero@gmail.com>
2024-06-11 21:44:35 -0300

    adding slider and config for gallery

--- jobsityApp/src/app/components/gallery/gallery.component.ts
+++ jobsityApp/src/app/components/gallery/gallery.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-gallery',
   standalone: true,
-  imports: [],
+  imports: [CarouselModule],
   templateUrl: './gallery.component.html',
   styleUrl: './gallery.component.scss'
 })
 export class GalleryComponent {
-
+  customOptions: OwlOptions = {
+    navText: ['PREV', 'NEXT'],
+    center: true,
+    items: 2,
+    loop: true,
+    dots:false,
+    autoHeight: true,
+    autoWidth: true,
+    margin: 20,
+    nav: true,
+    autoplay: true,
+    rtl: true,
+    skip_validateItems: true,
+    navSpeed: 700,
+    responsive: {
+      600: {
+        items: 4
+      }
+    }
+  }
 }
