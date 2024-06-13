dc916b0f35cc592fc49714c6de96882ca5d62867
unknown <fmmolinero@gmail.com>
2024-06-12 23:09:59 -0300

    adding fixes

--- jobsityApp/src/app/components/header/header.component.ts
+++ jobsityApp/src/app/components/header/header.component.ts
@@ -1,13 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  menuOpen = false;
 
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
 }
