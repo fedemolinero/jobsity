a6cd5dd784b356285eef8b650e9bbc1ef0b175a8
unknown <fmmolinero@gmail.com>
2024-06-20 13:25:29 -0300

    adding banner button navigation and fixing image route

--- jobsityApp/src/app/components/banner/banner.component.ts
+++ jobsityApp/src/app/components/banner/banner.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
-import { Router, RouterModule } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faChevronCircleDown, faChevronDown, faStar } from '@fortawesome/free-solid-svg-icons';
+import { faChevronDown, faStar } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-banner',
   standalone: true,
   imports: [
     FontAwesomeModule,
-    RouterModule
   ],
   templateUrl: './banner.component.html',
   styleUrl: './banner.component.scss'
@@ -18,10 +16,6 @@ export class BannerComponent {
   faStar = faStar;
 
   constructor(
-    private router: Router
   ) { }
 
-  gotoProgram() {
-    this.router.navigate(['#program']);
-  }
 }
